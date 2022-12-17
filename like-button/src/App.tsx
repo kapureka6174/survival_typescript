45facfbdcd4d8368c2234a83906be10f01153867
kapureka6174 <mitsu.n.0109@gmail.com>
2022-12-17 15:57:16 +0900

    いいねボタンの追加

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">TypeScriptはいいぞ</header>
+      <header className="App-header">
+        TypeScriptはいいぞ
+        <LikeButton />
+      </header>
     </div>
+  );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
   );
 }
 
